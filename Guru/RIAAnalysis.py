6142a3a90b3f1c9f67283a3bdfa3704a4c64139a
“DurjaMan27” <varunvellorerao@gmail.com>
2019-01-21 22:56:28 +0000

    RIA Analysis Horizontal Bar Chart

--- Guru/RIAAnalysis.py
+++ Guru/RIAAnalysis.py
@@ -8,12 +8,23 @@ gc = pygsheets.authorize(service_file='/home/ec2-user/environment/GoogleCloudEva
 df = pd.DataFrame()
 
 #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
-sh = gc.open_by_key('10dS74MpTQqt1Rc2hKDaRkkeapOBBxmgC8EXkj5WUM_U')
+riagooglesheet = gc.open_by_key('10dS74MpTQqt1Rc2hKDaRkkeapOBBxmgC8EXkj5WUM_U')
 
-#select the first sheet 
-wks = sh[0]
+#select the raw data sheet 
+riarawdata = riagooglesheet[0]
+
+#select the state distribution data sheet
+riastates = riagooglesheet[2]
 
 #update the first sheet with df, starting at cell B2. 
-df = wks.get_as_df()
+riastatesdf = riastates.get_as_df()
 
-print(df.count())
+#riastateslist = riastatesdf.range('A1:B55')  # get a range of cells 
+
+#print the df
+print(riastatesdf)
+
+#plot ria main office state count
+riastatesdfplot = riastatesdf.plot(kind='barh', x='Main Office State', y='COUNTA of Main Office State', width=0.85, figsize=(8, 10), legend=True)
+riastatesdfplotfig = riastatesdfplot.get_figure()
+riastatesdfplotfig.savefig('/home/ec2-user/environment/GlenPiGeeks/Guru/riastates.png')  
